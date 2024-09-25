ee47cdd043e36ce6e5443c0f740676c2b008b55d
didiermauricio~ <didiermauricio@gmail.com~>
2024-09-26 01:20:11 +0200

    New Examples Added

--- cypress/e2e/Steps/LoginSteps.js
+++ cypress/e2e/Steps/LoginSteps.js
@@ -5,36 +5,46 @@ Given('I access to login page', ()=>{
 })
 
 When('Incorrect User and password are typed', ()=>{
-  cy.get('#email').clear();
+  cy.get('#email').should("be.visible").clear();
   cy.get('#email').type("test@test.com");
   cy.get('#password').clear();
   cy.get('#password').type("123456789");
+  cy.get('#submit').should("be.visible").click() ;
+  
+})
 
-  cy.get('#submit').click();
+When('Incorrect User {string} and Password {string} are typed', (user, password)=>{
+  cy.get('#email').should("be.visible").clear();
+  cy.get('#email').type(user);
+  cy.get('#password').clear();
+  cy.get('#password').type(password);
+  cy.get('#submit').should("be.visible").click() ;
+  
 })
 
 Then('Error Message is displayed', ()=>{
-  cy.get('#error').contains("Incorrect username or password")
+  cy.get('#error').should('contain',"Incorrect username or password")
 })
 
 When('Incorrect User and password are not typed', ()=>{
   cy.get('#email').clear();
   cy.get('#password').clear();
 
-  cy.get('#submit').click();
+  cy.get('#submit').should("be.visible").click() ;
 })
 
 When('Correct User and password are typed', ()=>{
   cy.get('#email').clear();
+  cy.get('#email').should('be.empty');
   cy.get('#email').type("didiermauricio@gmail.com");
   cy.get('#password').clear();
   cy.get('#password').type("1032386452");
 
-  cy.get('#submit').click();
+  cy.get('#submit').should("be.visible").click() ; 
 })
 
 Then('Contact List is Displayed', ()=>{
-  cy.get('h1').contains("Contact List");
+  cy.get('h1').should('contain',"Contact List");
   cy.get('#add-contact').should("exist");
   cy.get('#logout').should("exist");
 })
